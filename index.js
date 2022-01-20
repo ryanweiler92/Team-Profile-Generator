773f723318df2ee9c88596e65f56a777791b1af9
ryanweiler92 <ryanweiler92@gmail.com>
2022-01-20 12:15:00 -0500

    employee prompt

--- index.js
+++ index.js
@@ -1,108 +1,59 @@
 const inquirer = require('inquirer');
 
-let employeeCount = 0;
-
-const managerPrompt = () => {
+const employeePrompt = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'teamManagerName',
-            message: 'Please enter the team manager name. (Required)',
-            validate: teamManagerNameInput => {
-                if (teamManagerNameInput) {
+            name: 'employeeName',
+            message: 'Please enter the name of the employee. (Required)',
+            validate: employeeNameInput => {
+                if (employeeNameInput) {
                     return true
                 } else {
-                    console.log('Please enter the team manager name.')
+                    console.log('No employee name was entered.')
                     return false;
                 }
             }
         },
         {
             type: 'input',
-            name: 'teamManagerId',
-            message: 'Please enter the team manager employee ID. (Required)',
-            validate: teamManagerIdInput => {
-                if (teamManagerIdInput) {
+            name: 'employeeId',
+            message: 'Please enter the employee ID. (Required)',
+            validate: employeeIdInput => {
+                if (employeeIdInput) {
                     return true
                 } else {
-                    console.log('Please enter the team manager employee ID.')
+                    console.log('No employee ID was entered.')
                     return false;
                 }
             }
         },
         {
             type: 'input',
-            name: 'teamManagerEmail',
+            name: 'employeeEmail',
             message: 'Please enter the team manager email address. (Required)',
-            validate: teamManagerEmailInput => {
-                if (teamManagerEmailInput) {
+            validate: employeeEmailInput => {
+                if (employeeEmailInput) {
                     return true
                 } else {
-                    console.log('Please enter the team manager email address.')
+                    console.log('Please enter the employee email address.')
                     return false;
                 }
             }
         },
         {
-            type: 'input',
-            name: 'teamManagerOffice',
-            message: 'Please enter the team manager office number. (Required)',
-            validate: teamManagerOfficeInput => {
-                if (teamManagerOfficeInput) {
-                    return true
-                } else {
-                    console.log('Please enter the team manager office number.')
-                    return false;
-                }
-            }
+            type: 'list',
+            name: 'role',
+            message: "What is the role of this employee? (Required)",
+            choices: ['Engineer', 'Intern', 'Manager']
         }
     ])
 };
 
-const employeeRolePrompt = employeeData => {
-    if (!employeeData.employees) {
-        employeeData.employees = [];
-    }
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'role',
-            message: "Would you like to enter a new engineer or intern? (Required)",
-            choices: ['Engineer', 'Intern', 'No']
-        }
-    ])
-    .then(roleData => {
-        employeeData.employees(employeeCount+1).push(roleData)
-        console.log(employeeData)
-
-    })
-    // .then(employeeData => {
-    //     console.log(employeeData)
-    //     if (employeeData.role == 'Engineer'){
-    //         return engineerPrompt(employeeData)
-    //     } else if (employeeData.role == 'Intern'){
-    //         return internPrompt == 'Intern'
-    //     } else {
-    //         return
-    //     }
-    // })
-
-}
 
 
 
-
-
-
-
-
-
-
-
-
-
-managerPrompt()
-.then(employeeRolePrompt)
-// .then (employeeData => {
-//     console.log(employeeData)
-// })
+employeePrompt()
+.then (employeeData => {
+    console.log(employeeData)
+})
