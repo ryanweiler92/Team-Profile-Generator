703167fa5645ba6fe028e62e48398413b0eea547
ryanweiler92 <ryanweiler92@gmail.com>
2022-01-20 15:18:08 -0500

    employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -30,4 +30,11 @@ test('get employee email address', () => {
     const employee = new Employee("Jon Snow", "1", "jonsnow@aim.com")
 
     expect(employee.getEmail()).toEqual("jonsnow@aim.com")
-})
+});
+
+test('get employee role', () => {
+    
+    const employee = new Employee("Jon Snow", "1", "jonsnow@aim.com")
+
+    expect(employee.getRole()).toEqual("employee")
+});
