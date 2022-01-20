f811dd4f53ea6638db39615e1542e6dd3d03cf81
ryanweiler92 <ryanweiler92@gmail.com>
2022-01-20 15:14:15 -0500

    employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,11 +8,26 @@ test('creates an employee object', () => {
     expect(employee.employeeId).toEqual(expect.any(String));
     expect(employee.email).toEqual(expect.any(String));
 
-}) 
+});
 
 test('get employee name', () => {
 
     const employee = new Employee("Jon Snow", "1", "jonsnow@aim.com")
 
     expect(employee.getName()).toEqual("Jon Snow")
+});
+
+test('get employee id', () => {
+
+    const employee = new Employee("Jon Snow", "1", "jonsnow@aim.com")
+
+    expect(employee.getId()).toEqual("1")
+
+});
+
+test('get employee email address', () => {
+
+    const employee = new Employee("Jon Snow", "1", "jonsnow@aim.com")
+
+    expect(employee.getEmail()).toEqual("jonsnow@aim.com")
 })
